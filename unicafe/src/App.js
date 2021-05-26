b53dedd691a429b2e8b7cf266c289de439d1e922
Mohana Vamsi <52503554+mohanavamsi20@users.noreply.github.com>
2021-05-27 01:25:30 +0530

    1.9 completed

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -36,17 +36,33 @@ const App = () => {
   const setTobad= newBad => {
     setBad(newBad)
   }
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button handleClick={() => setTogood(good + 1)} text="good" />
-      <Button handleClick={() => setToneutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setTobad(bad + 1)} text="bad" />
-
-      <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
-    </div>
-  )
+  const total = good + neutral + bad;
+  if(total === 0){
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => setTogood(good + 1)} text="good" />
+        <Button handleClick={() => setToneutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setTobad(bad + 1)} text="bad" />
+  
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  else{
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => setTogood(good + 1)} text="good" />
+        <Button handleClick={() => setToneutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setTobad(bad + 1)} text="bad" />
+  
+        <h1>statistics</h1>
+        <Statistics good={good} neutral={neutral} bad={bad}/>
+      </div>
+    )
+  }
 }
 
 export default App
