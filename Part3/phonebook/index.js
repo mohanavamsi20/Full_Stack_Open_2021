b4c0937fa2189b48e76e3e99a16b1ef5677e8700
Mohana Vamsi <52503554+mohanavamsi20@users.noreply.github.com>
2021-06-05 13:10:53 +0530

    3.4 completed

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -48,6 +48,13 @@ let persons = [
     }
   })
   
+  app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+  
 
 const PORT = 3001
 app.listen(PORT)
