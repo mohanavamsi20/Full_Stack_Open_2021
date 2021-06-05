80942d3750277f9d8522e8cdfa97774bb81a6409
Mohana Vamsi <52503554+mohanavamsi20@users.noreply.github.com>
2021-06-05 12:56:28 +0530

    3.1 completed

---
+++ Part3/phonebook/index.js
@@ -0,0 +1,38 @@
+
+const http = require('http')
+const express = require('express')
+const app = express()
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "645-568-2014"
+      },
+      {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+      },
+      {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+      },
+      {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+        
+      }
+  ]
+  app.get('/', (request, response) => {
+    response.send('<h1>Welcome to phonebook!</h1>')
+  })
+  
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
