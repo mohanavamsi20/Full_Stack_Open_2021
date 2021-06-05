479aabbcbc9b9eee496f273c582ab52c4f85cd2d
Mohana Vamsi <52503554+mohanavamsi20@users.noreply.github.com>
2021-06-05 13:05:24 +0530

    3.2 completed

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -33,6 +33,11 @@ let persons = [
     response.json(persons)
   })
 
+  app.get('/info',(request,response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date}</p>`)
+  })
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
