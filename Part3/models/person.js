e0c44cd07ca59ed6cf8a3ad5698b1ac2959df87c
Mohana Vamsi <52503554+mohanavamsi20@users.noreply.github.com>
2021-06-11 12:28:45 +0530

    part 3 final update

--- Part3/models/person.js
+++ Part3/models/person.js
@@ -1,22 +1,24 @@
+/* eslint-disable linebreak-style */
 const mongoose = require('mongoose')
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
-const uniqueValidator = require('mongoose-unique-validator')
+//const uniqueValidator = require('mongoose-unique-validator')
 
 
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const personSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true, minlength: [3, 'Name must have atleast 3 characters'] },
-    number: { type: String, required: true, unique: true, minlength: [8, 'Name must have atleast 8 characters'] },
-  })
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true, minlength: [3, 'Name must have atleast 3 characters'] },
+  number: { type: String, required: true, unique: true, minlength: [8, 'Name must have atleast 8 characters'] },
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
