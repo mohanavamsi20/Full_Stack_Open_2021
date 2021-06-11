405e49f58ef935fe579e607fd4822e4fa5c39700
Mohana Vamsi <52503554+mohanavamsi20@users.noreply.github.com>
2021-06-11 09:28:15 +0530

    part3 updated

--- Part3/index.js
+++ Part3/index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+/* eslint-disable linebreak-style */
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
@@ -18,99 +18,76 @@ morgan.token('post', (request) => {
 morgan.format('postFormat', ':method :url :status :res[content-length] - :response-time ms :post')
 app.use(morgan('postFormat'))
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "645-568-2014"
-      },
-      {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-      },
-      {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-      },
-      {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-      }
-  ]
   
-  app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
   })
+})
 
-  app.get('/info', (request, response) => {
-    Person.find({}).then(persons => {
-      response.send(`
+app.get('/info', (_request, response) => {
+  Person.find({}).then(persons => {
+    response.send(`
           <p>Phonebook has info for ${persons.length} people</p>
           <p>${new Date}</p>
           `)
-    })
   })
+})
 
-  app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
-    })
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
   })
+})
 
-  const randID = () => {
-    const maxId = Math.floor((Math.random() * 100) + 0);
-    return maxId
+  
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'name or number missing' })
   }
   
-  app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
   
-    if (body.name === undefined || body.number === undefined) {
-      return response.status(400).json({ error: 'name or number missing' })
-    }
-  
-    const person = new Person({
-      name: body.name,
-      number: body.number
+  person.save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedPerson => {
+      response.json(savedAndFormattedPerson)
     })
-  
-    person.save()
-      .then(savedPerson => savedPerson.toJSON())
-      .then(savedAndFormattedPerson => {
-        response.json(savedAndFormattedPerson)
-      })
-      .catch(error => next(error))
-  })
+    .catch(error => next(error))
+})
   
 
-  app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-      .then(() => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
   
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-  app.use(errorHandler)
+  
+  next(error)
+}
+app.use(errorHandler)
 
 
+
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
