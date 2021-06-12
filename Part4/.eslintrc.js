229e517906d41e95e3e93ceb0c9f9121971f7aa6
Mohana Vamsi <52503554+mohanavamsi20@users.noreply.github.com>
2021-06-12 10:34:58 +0530

    4.1 - 4.3 completed

---
+++ Part4/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true,
+        'node': true,
+        'jest': true,
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 12
+    },
+    'plugins': [
+        'react'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
