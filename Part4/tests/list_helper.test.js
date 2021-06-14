d6a6078c1886efb370f6a7ab2f1c08a7c21d8a32
Mohana Vamsi <52503554+mohanavamsi20@users.noreply.github.com>
2021-06-14 16:54:56 +0530

    4.5,4.6,4.7 completed

--- Part4/tests/list_helper.test.js
+++ Part4/tests/list_helper.test.js
@@ -1,16 +1,7 @@
-const listHelper = require ('../utils/list_helper')
-describe('total likes', () => {
-    const listWithOneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        }
-    ]
-  
+const listHelper = require('../utils/list_helper')
+
+describe('most likes', () => {
+
     const listWithManyBlogs = [
         {
             _id: '5a422a851b54a676234d17f7',
@@ -61,19 +52,15 @@ describe('total likes', () => {
             __v: 0
         }
     ]
-  
-    test('of empty list is zero', () => {
-        const result = listHelper.totalLikes([])
-        expect(result).toBe(0)
+
+    test('is identified correctly', () => {
+        const result = listHelper.mostLikes(listWithManyBlogs)
+        const winner = {
+            author: 'Edsger W. Dijkstra',
+            likes: 17
+        }
+
+        expect(result).toEqual(winner)
     })
-  
-    test('when list has only one blog equals the likes of that', () => {
-        const result = listHelper.totalLikes(listWithOneBlog)
-        expect(result).toBe(5)
-    })
-  
-    test('of a bigger list is calculated right', () => {
-        const result = listHelper.totalLikes(listWithManyBlogs)
-        expect(result).toBe(36)
-    })
+
 }) 
