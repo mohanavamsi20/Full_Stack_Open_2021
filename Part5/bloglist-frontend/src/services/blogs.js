c8e1a5d62677e0505b00545d751fd225991347de
Mohana Vamsi <52503554+mohanavamsi20@users.noreply.github.com>
2021-06-17 09:09:21 +0530

    part5 v1.0

---
+++ Part5/bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const all = {
+  getAll: getAll
+}
+
+export default all;
