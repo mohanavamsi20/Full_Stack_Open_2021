32071475ce7e1556cedca14f464dfcd17eb16444
Mohana Vamsi <52503554+mohanavamsi20@users.noreply.github.com>
2021-06-18 17:13:33 +0530

    commited

---
+++ Part7/bloglist-backend/build/precache-manifest.ae7a7d00930327950757b3a63ed1bfa9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b61743ed465d3264e3f1d6247afec4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87cb61a8f7a38d5e719d",
+    "url": "/static/js/2.f317cc12.chunk.js"
+  },
+  {
+    "revision": "42fe05a0a986d98cda00",
+    "url": "/static/js/main.30687446.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
